626997c6c59024ba81001cbf7442181842c56c53
Jackson Brenneman <jdb360@georgetown.edu>
2025-10-26 10:45:29 -0400

    mmap proto

--- newlib/libc/sys/lilac/sys/mman.h
+++ newlib/libc/sys/lilac/sys/mman.h
@@ -1,4 +1,18 @@
 #ifndef _SYS_MMAN_H
 #define _SYS_MMAN_H
 
+#include <sys/types.h>
+#include <stddef.h>
+
+#define PROT_READ  0x1
+#define PROT_WRITE 0x2
+#define PROT_EXEC  0x4
+
+#define MAP_SHARED  0x01
+#define MAP_PRIVATE 0x02
+#define MAP_ANONYMOUS 0x20
+
+void * mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+int munmap(void *addr, size_t length);
+
 #endif
